fe77c34fbcd04cb804de4f83221350e196c04af3
Max Appleton <maxiappleton@gmail.com>
2018-05-07 11:58:34 -0500

    set up Project CRUD functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,54 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = 'Your project has been created!'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Could not create project'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      flash[:notice] = 'Your project was updated!'
+      redirect_to project_path(@project)
+    else
+      flash[:alert] = 'Could not update project'
+      render :edit
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+end
